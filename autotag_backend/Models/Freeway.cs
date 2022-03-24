9b18b8ea2bf36eb221cab0431f04c91dae7a325b
erickmsdev <erick.ms.dev@gmail.com>
2022-03-24 13:59:32 -0300

    agregar dimension fecha y freeway

---
+++ autotag_backend/Models/Freeway.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoTagBackEnd.Models
+{
+    public partial class Freeway
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public int PortalId { get; set; }
+        public string? ExternalCode { get; set; }
+        public string Name { get; set; } = null!;
+        public int Order { get; set; }
+
+        public virtual Portal Portal { get; set; } = null!;
+    }
+}
